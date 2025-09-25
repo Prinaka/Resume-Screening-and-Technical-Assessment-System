263f30370a0b548fcba6961cd91d2ae4e63103da
Prinaka Chakraborty <prinaka.4002@gmail.com>
2025-09-26 03:04:27 +0530

    Update pdf_report.py

--- helpers/pdf_report.py
+++ helpers/pdf_report.py
@@ -5,7 +5,7 @@ from fpdf import FPDF
 def generate_pdf_report(questions, answers, grades):
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font("Arial", size=12)
+    pdf.set_font("Arial", "B", size=12)
 
     pdf.cell(200, 10, txt="Technical Assessment Report", ln=True, align='C')
     pdf.ln(10)
@@ -15,21 +15,21 @@ def generate_pdf_report(questions, answers, grades):
         score = data["score"]
         feedback = data["feedback"]
 
-        pdf.set_font("Arial", 'B', 12)
+        pdf.set_font("Arial", 'B', 11)
         pdf.multi_cell(0, 10, txt=f"Question {i}:")
-        pdf.set_font("Arial", '', 12)
+        pdf.set_font("Arial", '', 11)
         pdf.multi_cell(0, 10, txt=ques)
         pdf.ln(2)
 
-        pdf.set_font("Arial", 'B', 12)
+        pdf.set_font("Arial", 'B', 11)
         pdf.multi_cell(0, 10, txt="Your Answer:")
-        pdf.set_font("Arial", '', 12)
+        pdf.set_font("Arial", '', 11)
         pdf.multi_cell(0, 10, txt=ans)
         pdf.ln(2)
 
-        pdf.set_font("Arial", 'B', 12)
+        pdf.set_font("Arial", 'B', 11)
         pdf.multi_cell(0, 10, txt=f"Score: {score}%")
-        pdf.set_font("Arial", '', 12)
+        pdf.set_font("Arial", 'B', 11)
         pdf.multi_cell(0, 10, txt=f"Feedback: {feedback}")
         pdf.ln(5)
 
